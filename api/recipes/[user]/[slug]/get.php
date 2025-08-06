189af3e6122ad99defd0e8e40f0664431f47da85
Lewis Milburn <lewiscmilburn@gmail.com>
2025-08-06 21:55:46 +0100

    Update recipe API

--- api/recipes/[user]/[slug]/get.php
+++ api/recipes/[user]/[slug]/get.php
@@ -9,10 +9,32 @@ $uriParts = explode('/',$_SERVER['REQUEST_URI']);
 $recipeSlug = $db->escape($uriParts[array_key_last($uriParts)]);
 $username = $db->escape($uriParts[array_key_last($uriParts)-1]);
 
-$query = $db->query("SELECT * FROM recipes WHERE slug = '".$recipeSlug."' AND `uuid` = '".new Users()->usernameToUuid($username)."' AND `visibility` = 3");
+$query = $db->query("SELECT `uuid`, `title`, `description`, `ingredients`, `steps`, `prep`, `cook`, `servings`, `vegetarian`, `vegan`, `dairy_free`, `gluten_free`, `difficulty`, `date` FROM recipes WHERE slug = '".$recipeSlug."' AND `uuid` = '".new Users()->usernameToUuid($username)."' AND `visibility` = 3");
 
 if ($query->num_rows > 0) {
-    $results = $query->fetch_all(MYSQLI_ASSOC);
+    $results = $query->fetch_object();
+
+    $results->author = [
+        "name" => new Users()->uuidToName($results->uuid),
+        "username" => new Users()->uuidToUsername($results->uuid)
+    ];
+
+    $results->dietary = [
+        "dairy_free" => $results->dairy_free,
+        "gluten_free" => $results->gluten_free,
+        "vegan" => $results->vegan,
+        "vegetarian" => $results->vegetarian
+    ];
+
+    $results->time = [
+        "prep" => $results->prep,
+        "cook" => $results->cook
+    ];
+
+    $results->ingredients = json_decode(html_entity_decode($results->ingredients));
+    $results->steps = json_decode(html_entity_decode($results->steps));
+
+    unset($results->uuid, $results->dairy_free, $results->gluten_free, $results->vegan, $results->vegetarian, $results->cook, $results->prep);
 
     echo json_encode([
         "status" => ["code" => "200 OK", "message" => null],
