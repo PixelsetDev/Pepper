4f841a47f432f38b38a7eebdf505e91308059cad
Lewis Milburn <lewiscmilburn@gmail.com>
2025-08-06 21:09:37 +0100

    Add more collection info

--- api/collections/[id]/get.php
+++ api/collections/[id]/get.php
@@ -1,8 +1,11 @@
 <?php
 
+use Pepper\Helpers\Users;
 use Starlight\Database\SQL;
 
 $db = new SQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+$userHelper = new Users();
 
 $uriParts = explode('/',$_SERVER['REQUEST_URI']);
 
@@ -12,6 +15,34 @@ $query = $db->query("SELECT `name`, `description`, `slug`, `author`, `featured` 
 
 if ($query->num_rows > 0) {
     $result = $query->fetch_object();
+
+    $query2 = $db->query("SELECT `slug`, `author`, `title`, `notes` FROM `collections_recipes` WHERE `collection` = '".$slug."'");
+    if ($query2->num_rows > 0) {
+        $result->recipes = $query2->fetch_all(MYSQLI_ASSOC);
+
+        foreach ($result->recipes as $key => $recipe) {
+            $result->recipes[$key]['author'] = [
+                "name" => $userHelper->uuidToName($result->recipes[$key]['author']),
+                "username" => $userHelper->uuidToUsername($result->recipes[$key]['author'])
+            ];
+        }
+    }
+
+    $userQuery = $db->query("SELECT `avatar` FROM `users` WHERE `uuid` = '".$db->escape($result->author)."'");
+    if ($userQuery->num_rows > 0) {
+        $result->author = [
+            "name" => $userHelper->uuidToName($result->author),
+            "username" => $userHelper->uuidToUsername($result->author),
+            "avatar" => $userQuery->fetch_object()->avatar
+        ];
+    } else {
+        $result->author = [
+            "name" => $userHelper->uuidToName($result->author),
+            "username" => $userHelper->uuidToUsername($result->author),
+            "avatar" => 'https://data.portalsso.com/avatar/missing.webp'
+        ];
+    }
+
     echo json_encode([
         "status" => ["code" => "200 OK", "message" => null],
         "data" => $result,
