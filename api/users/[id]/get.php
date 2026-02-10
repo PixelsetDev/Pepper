a0b9632281738f8bb3ed027eb29f4417bd2f01de
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2026-02-10 12:31:04 +0000

    Bump changes

--- api/users/[id]/get.php
+++ api/users/[id]/get.php
@@ -1,16 +1,14 @@
 <?php
 
-use Starlight\Database\SQL;
+use Starlight\Database\MySQL;
 
-$db = new SQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$db = new MySQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 $uriParts = explode('/',$_SERVER['REQUEST_URI']);
-$username = $db->escape($uriParts[array_key_last($uriParts)]);
 
-$query = $db->query("SELECT `name`, `username`, `uuid` FROM `user` WHERE `username` = '".$username."'");
+$user = $db->fetchOne("SELECT `name`, `username`, `uuid` FROM `user` WHERE `username` = ?",[$uriParts[array_key_last($uriParts)]]);
 
-if ($query->num_rows > 0) {
-    $user = $query->fetch_object();
+if ($db->numRows() > 0 || $user === null) {
     echo '{"status": {"code": "200 OK", "message": null}, "data": '.json_encode($user).'}';
 } else {
     echo '{"status": {"code": "404 Not Found", "message": "The requested user could not be found."}, "data": null}';
--- api/users/get.php
+++ api/users/get.php
@@ -1,13 +1,12 @@
 <?php
 
-use Starlight\Database\SQL;
+use Starlight\Database\MySQL;
 
-$db = new SQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$db = new MySQL(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-$query = $db->query('SELECT `name`, `username`, `uuid` FROM `user` WHERE 1');
+$users = $db->fetchAll('SELECT `name`, `username`, `uuid` FROM `user` WHERE 1');
 
-if ($query->num_rows > 0) {
-    $users = $query->fetch_all(MYSQLI_ASSOC);
+if ($db->numRows() > 0 || $users === null) {
     echo '{"status": {"code": "200 OK", "message": null}, "data": '.json_encode($users).'}';
 } else {
     echo '{"status": {"code": "404 Not Found", "message": "No users were found in the database."}, "data": null}';
